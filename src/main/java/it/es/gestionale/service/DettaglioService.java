8a1d0bc1547f5dfc379008922cf3e25155f64b73
Teresadi <96294922+Teresadi@users.noreply.github.com>
2022-03-11 16:44:52 +0100

    DettaglioService

---
+++ src/main/java/it/es/gestionale/service/DettaglioService.java
@@ -0,0 +1,36 @@
+package it.es.gestionale.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.es.gestionale.model.DettaglioEntity;
+import it.es.gestionale.model.DettaglioKey;
+import it.es.gestionale.repository.DettaglioDB;
+
+
+
+@Service
+public class DettaglioService {
+	
+	@Autowired
+	DettaglioDB dbEdi;
+	
+	public List<DettaglioEntity> getLista() {
+	return dbEdi.findAll();	//Passacarte
+	}
+	
+	public DettaglioEntity save(DettaglioEntity e){
+	return dbEdi.save(e);
+	}
+
+	public void delete(DettaglioKey id) {
+	dbEdi.delete(dbEdi.getById(id));
+	}
+	
+	public DettaglioEntity getByid(int id) {
+	return dbEdi.findById(id).orElse(new DettaglioEntity());
+	}
+
+}
