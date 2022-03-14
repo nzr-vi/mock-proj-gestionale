2bb82b689d02ebb4b3962e6d180028fb3211cb85
LeoGiardino <panto.giardino@gmail.com>
2022-03-14 14:45:04 +0100

    modifica

--- src/main/java/it/es/gestionale/presentation/ArticoloMVC.java
+++ src/main/java/it/es/gestionale/presentation/ArticoloMVC.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,10 +40,25 @@ public class ArticoloMVC {
 
 	@GetMapping("/add") 
 	public String addForm(Model model) {
+        
 	
 		model.addAttribute("articolo", new ArticoloEntity());
 		
 		return "addArticolo";
 	}
     
+	@GetMapping("/{id}")
+	public String modifica(@PathVariable("id") int id, Model model) { 
+
+		ArticoloEntity articolo = srv.getById(id);
+		
+		if(articolo==null)
+			return "redirect:/home";
+		
+		
+		model.addAttribute("articolo", articolo); 
+		
+		return "addArticolo";	
+	}
+	
 }
