c7a68dd05564e756df177ba8d315120be5a24250
nzrv <96348033+nzr-vi@users.noreply.github.com>
2022-03-14 14:50:30 +0100

    added user check

--- src/main/java/it/es/gestionale/integration/ArticoloREST.java
+++ src/main/java/it/es/gestionale/integration/ArticoloREST.java
@@ -11,34 +11,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import it.es.gestionale.model.ArticoloEntity;
+import it.es.gestionale.model.UtenteEntity;
+import it.es.gestionale.model.UtenteEntity.Role;
 import it.es.gestionale.service.ArticoloService;
 
 @RestController
 @RequestMapping("/api/articolo")
+@SessionAttributes("utente")
 public class ArticoloREST {
 
+	@Autowired
+	ArticoloService srv;
 
-    @Autowired
-    ArticoloService srv;
-
-    @GetMapping
-    public List<ArticoloEntity> allArticoli(){
-        return srv.findAll();
-    }
-
+	@GetMapping
+	public List<ArticoloEntity> allArticoli() {
+		return srv.findAll();
+	}
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = "application/json")
-	public ResponseEntity<ArticoloEntity> save(@RequestBody ArticoloEntity a) { 
-            
+	public ResponseEntity<ArticoloEntity> save(@SessionAttribute(name = "utente") UtenteEntity utente,
+			@RequestBody ArticoloEntity a) {
 
-                a = srv.save(a);
-
-                return new ResponseEntity<ArticoloEntity>(a, HttpStatus.OK);
-					
+		if(utente.getRuolo()==Role.supervisore)
+		{
+			a = srv.save(a);
+			return new ResponseEntity<ArticoloEntity>(a, HttpStatus.OK);	
 		}
-		
+		return ResponseEntity.badRequest().build();
 	}
-    
-
+}
--- src/main/java/it/es/gestionale/presentation/ArticoloMVC.java
+++ src/main/java/it/es/gestionale/presentation/ArticoloMVC.java
@@ -1,17 +1,24 @@
 package it.es.gestionale.presentation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import it.es.gestionale.model.ArticoloEntity;
+import it.es.gestionale.model.UtenteEntity;
+import it.es.gestionale.model.UtenteEntity.Role;
 import it.es.gestionale.service.ArticoloService;
 
 @Controller
 @RequestMapping("/home")
+@SessionAttributes("utente")
 public class ArticoloMVC {
 
     @Autowired
@@ -28,12 +35,15 @@ public class ArticoloMVC {
 
    
 	@PostMapping("/save") 
-	public String saveStudente(ArticoloEntity a) {
-		// salva studente 
-		srv.save(a);
+	public String saveStudente(@SessionAttribute(name = "utente") UtenteEntity utente,
+			ArticoloEntity a) {
 		
+		if(utente.getRuolo()==Role.supervisore)
+		{
+			a = srv.save(a);
+		}
 		
-		return "redirect:/home"; 
+		return "redirect:''"; 
 	}
 	
 
